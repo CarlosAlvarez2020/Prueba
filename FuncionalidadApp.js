910cc2f46c4e78c8588217676f214093848c052d
CarlosAlvarez2020 <cradman45@gmail.com>
2020-10-14 08:52:21 -0500

    Creacion de archivos frontend

---
+++ FuncionalidadApp.js
@@ -0,0 +1,23 @@
+alert("Funciona");
+var provider=new firebase.auth.GoogleAuthProvider();
+$('#logueo').click(function(){
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then(function(result) {
+        console.log(result.user);
+        almacenaDatos(result.user);
+        $('#logueo').hide();
+        $('#principal').append("<img src='"+result.user.photoURL+"'/>")        
+    });
+});
+
+function almacenaDatos(user){
+    var datosDelUsuario={
+        nombre:user.displayName,
+        email:user.email,
+        foto:user.photoURL,
+        uid:user.uid
+    }
+    firebase.database().ref("datosInicioDeSesion/"+user.uid)
+    .push(datosDelUsuario)
+}
